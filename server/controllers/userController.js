ab44f565d3c653e1ece844a4ac49842ab6edb5ed
alexboliam <alexboliam@gmail.com>
2022-03-24 13:51:11 +0200

    add jwt auth and middleware

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,13 @@
 const ApiError = require('../errors/apiError')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const { User } = require('../models/models')
 
+const generateJwt = (id, username, fullname) => {
+    return jwt.sign({ id, username, fullname },
+        process.env.SECRET_KEY, { expiresIn: '14d' }
+    )
+}
 class UserController {
     async register(req, res, next) {
         const { username, fullname, password } = req.body
@@ -16,18 +22,28 @@ class UserController {
         }
 
         const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({ username, fullname: fullname, password: hashPassword })
+        const token = generateJwt(user.id, user.username, user.fullname)
+        return res.json({ token })
     }
 
-    async login(req, res) {
-
+    async login(req, res, next) {
+        const { username, password } = req.body
+        const user = await User.findOne({ where: { username } })
+        if (!user) {
+            return next(ApiError.internal('Неверные имя пользователя или пароль'))
+        }
+        let comparedPassword = bcrypt.compareSync(password, user.password)
+        if (!comparedPassword) {
+            return next(ApiError.internal('Неверные имя пользователя или пароль'))
+        }
+        const token = generateJwt(user.id, user.username, user.fullname)
+        return res.json({ token })
     }
 
     async check(req, res, next) {
-        const { id } = req.query
-        if (!id) {
-            return next(ApiError.badRequest('Id is missing'))
-        }
-        res.json(id)
+        const token = generateJwt(req.user.id, req.user.username, req.user.fullname)
+        return res.json({ token })
     }
 }
 
